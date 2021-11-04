161db6c4982a18f141921f79d581e266977236bc
AntonTsarenkoTR <anton.tsarenko@thomsonreuters.com>
2021-11-04 13:54:55 +0200

    feat(account_balances): Module 2 bonus task

---
+++ src/mappings/Balance.ts
@@ -0,0 +1,13 @@
+import {SubstrateEvent} from "@subql/types";
+import {TotalBalance} from "../types";
+import {Balance} from "@polkadot/types/interfaces";
+
+export async function handleTotalBalance(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [account, balance]}} = event;
+
+    let record = await TotalBalance.get(event.extrinsic.block.block.header.hash.toString());
+    record.totalBalance = record.totalBalance + (balance as Balance).toBigInt();
+    record.account = account.toString();
+    
+    await record.save();
+}
